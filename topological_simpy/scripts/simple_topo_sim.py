d646acbc1bc08e20ec0259503f2c09d54598a9ee
Marc Hanheide <marc@hanheide.net>
2021-02-14 17:52:03 +0000

    initial version of a topo simpy environment

---
+++ topological_simpy/scripts/simple_topo_sim.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from topological_simpy.robot import Robot, TopoMap
+import simpy
+import itertools
+
+
+from random import randint, choice
+
+env = simpy.Environment()
+#tmap = TopoMap('/home/mhanheide/workspace/topological_navigation_ws/src/topological_navigation/topological_navigation/maps/test.tmap2', env)
+tmap = TopoMap('/home/mhanheide/workspace/topological_navigation_ws/src/topological_navigation/topological_navigation/maps/riseholme.tmap2', env)
+
+#r1 = Robot('thorvald', tmap, 'A')
+#r1.goto('A')
+#r2 = Robot('thorvald2', tmap, 'D')
+#r2.goto('A')
+
+robots = [
+    #Robot('Hurga', tmap, 'A'),
+    #Robot('Foo', tmap, 'B'),
+    Robot('Hurga', tmap, 'dock_0'),
+    Robot('Foo', tmap, 'dock_1'),
+    Robot('Foo2', tmap, 'dock_2')
+]
+
+nodes = tmap.get_nodes()
+
+print(nodes)
+
+def goal_generator(env, robots, nodes, max_interval=50):
+    for i in itertools.count():
+        yield env.timeout(randint(env.now+1, env.now+max_interval))
+        r = choice(robots)
+        n = choice(nodes)
+        print('new goal for robot %s: %s' % (r._name, n))
+        r.goto(n)
+
+env.process(goal_generator(env, robots, nodes))
+
+until = 3600
+while env.peek() < until:
+    tmap.monitor()
+    for r in robots:
+        print("R% 3d:     STATUS: % 10s: %s (%s)" % (
+            env.now,
+            r._name,
+            r._current_node,
+            'ACTIVE' if r._active_process and r._active_process.is_alive else 'IDLE'
+        ))
+    env.step()
+print(tmap._node_log)
+#env.run(until=60)
--- topological_simpy/src/topological_simpy/robot.py
+++ topological_simpy/src/topological_simpy/robot.py
@@ -4,7 +4,12 @@ import itertools
 import random
 
 import simpy
+from yaml import safe_load
 
+from topological_navigation.route_search2 import TopologicalRouteSearch2
+from topological_navigation.tmap_utils import *
+
+from math import hypot, ceil
 
 RANDOM_SEED = 42
 GAS_STATION_SIZE = 200     # liters
@@ -15,6 +20,190 @@ REFUELING_SPEED = 2        # liters / second
 TANK_TRUCK_TIME = 30      # Seconds it takes the tank truck to arrive
 T_INTER = [1, 30]        # Create a car every [min, max] seconds
 SIM_TIME = 20000            # Simulation time in seconds
+
+
+
+from functools import partial, wraps
+import simpy
+
+def patch_resource(resource, pre=None, post=None):
+    """Patch *resource* so that it calls the callable *pre* before each
+     put/get/request/release operation and the callable *post* after each
+     operation.  The only argument to these functions is the resource
+     instance.
+
+    """
+    def get_wrapper(func):
+        # Generate a wrapper for put/get/request/release
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # This is the actual wrapper
+            # Call "pre" callback
+            if pre:
+                pre(resource)
+
+            # Perform actual operation
+            ret = func(*args, **kwargs)
+            
+            # Call "post" callback
+            if post:
+                post(resource)
+
+            return ret
+        return wrapper
+
+     # Replace the original operations with our wrapper
+    for name in ['put', 'get', 'request', 'release']:
+        if hasattr(resource, name):
+            setattr(resource, name, get_wrapper(getattr(resource, name)))
+
+class TopoMap():
+    def __init__(self, topo_map_file, env=None):
+        with open(topo_map_file, "r") as f:
+            self.tmap2 = safe_load(f)
+        self._route_planner = TopologicalRouteSearch2(self.tmap2)
+        self.env = env
+
+        self._nodes = sorted([n['node']['name'] for n in self.tmap2['nodes']])
+        self._nodes_dict = {n['node']['name']: n['node'] for n in self.tmap2['nodes']}
+        self._node_res = {}
+
+        self._node_log = {}
+
+        if self.env:
+            for n in self._nodes:
+                self._node_res[n] = simpy.Container(self.env, capacity=1, init=0)  # each node can hold one robot max
+                self._node_log[n] = []
+                patch_resource(self._node_res[n], post=partial(self.log_resource, n))
+
+    def log_resource(self, node, resource):
+        """This is our monitoring callback."""
+        item = (
+            resource._env.now,  # The current simulation time
+            resource.level
+        )
+        self._node_log[node].append(item)
+
+    def get_nodes(self):
+        return self._nodes
+
+    def distance(self, nodeA, nodeB):
+        a = self._nodes_dict[nodeA]
+        b = self._nodes_dict[nodeB]
+        return hypot(
+            a['pose']['position']['x']-b['pose']['position']['x'],
+            a['pose']['position']['y']-b['pose']['position']['y']
+            )
+
+    def request_node(self, node):
+        if self.env:
+            return self._node_res[node].put(1)
+
+    def release_node(self, node):
+        if self.env:
+            if self._node_res[node].level > 0:
+                return self._node_res[node].get(1)
+            else:
+                return self.env.timeout(0)
+
+    def get_route(self, origin, target):
+        return self._route_planner.search_route(origin, target)
+
+    def node_occupied(self, node):
+        return self._node_res[n].count > 0
+
+    def monitor(self, freq=2):
+        occupied_nodes = []
+        for n in self._node_res:
+            if self._node_res[n].level > 0:
+                occupied_nodes.append(n)
+        print("*% 4d:   nodes %s are occupied" % (self.env.now, sorted(occupied_nodes)))
+
+class Robot():
+    def __init__(self, name, tmap, initial_node='A'):
+        self._current_node = initial_node
+        self._tmap = tmap
+        self._env = tmap.env
+        self._name = name
+        self._speed_m_s = .3
+        self._active_process = None
+        if self._tmap.env:
+            self._tmap.request_node(initial_node)
+            #self._tmap._node_res[initial_node].count = 1
+            #print(self._current_node)
+            #self._tmap.env.process(self._goto(initial_node))
+
+    def _goto(self, target):
+        route = self._tmap.get_route(self._current_node, target)
+        if self._tmap.env:
+            if route is not None:
+                r = route.source[1:]
+            else:
+                r = []
+            r.append(target)
+            if target == self._current_node:
+                print('% 5d: %s is already at target %s' % (
+                    self._tmap.env.now, self._name, 
+                    target
+                ))
+                return
+
+            print('% 5d: %s going from node %s to node %s via route %s' % (
+                self._tmap.env.now, self._name, 
+                self._current_node, target, r))
+            interrupted = False
+            for n in r:
+                #  getting the distance between current and target:
+                d = self._tmap.distance(self._current_node, n)
+                time_to_travel = int(ceil(d / self._speed_m_s))
+                print('% 5d:  %s traversing route from node %s to node %s (distance: %f, time: %d)' % (
+                    self._tmap.env.now, self._name, self._current_node, n, d, time_to_travel))
+                request = None
+                try:
+                    start_wait = self._env.now
+                    yield self._tmap.request_node(n)
+                    if self._env.now - start_wait > 0: 
+                        print "$$$$ % 5d:  %s has lock on %s after %d" % (self._env.now, self._name, n, self._env.now - start_wait)
+                except:
+                    print('% 5d: %s INTERRUPTED while waiting to gain access to go from node %s going to node %s' % (
+                        self._tmap.env.now, self._name, 
+                        self._current_node, n
+                    ))
+                    interrupted = True
+                    break;
+                try:
+                    time_to_travel = ceil(d / (2*self._speed_m_s))
+                    yield self._tmap.env.timeout(time_to_travel)
+                    yield self._tmap.release_node(self._current_node)
+                    self._current_node = n
+                    print('% 5d:  %s reached node %s' % (self._tmap.env.now, self._name, n))
+                    yield self._tmap.env.timeout(0)
+                except simpy.Interrupt:
+                    print('% 5d: %s INTERRUPTED while travelling from node %s going to node %s' % (
+                        self._tmap.env.now, self._name, 
+                        self._current_node, n
+                    ))
+                    print('% 5d: @@@ %s release previously acquired target node %s' % (self._env.now, self._name, n))
+                    yield self._tmap.release_node(n)
+                    interrupted = True
+                    break
+                #if self._current_node != target:
+            if interrupted:
+                print('% 5d: %s ABORTED at %s' % (self._tmap.env.now, self._name, self._current_node))
+            else:
+                print('% 5d: %s COMPLETED at %s' % (self._tmap.env.now, self._name, self._current_node))
+            
+    def goto(self, target):
+        if self._tmap.env:
+            if self._active_process:
+                if self._active_process.is_alive:
+                    self._active_process.interrupt()
+                    self._active_process = None
+            self._active_process = self._tmap.env.process(self._goto(target))
+        else:
+            self._current_node = target
+
+            
 
 
 def car(name, env, gas_station, fuel_pump):
@@ -74,16 +263,19 @@ def car_generator(env, gas_station, fuel_pump):
         env.process(car('Car %d' % i, env, gas_station, fuel_pump))
 
 
-# Setup and start the simulation
-print('Gas Station refuelling')
-random.seed(RANDOM_SEED)
 
-# Create environment and start processes
-env = simpy.Environment()
-gas_station = simpy.Resource(env, 2)
-fuel_pump = simpy.Container(env, GAS_STATION_SIZE, init=GAS_STATION_SIZE)
-env.process(gas_station_control(env, fuel_pump))
-env.process(car_generator(env, gas_station, fuel_pump))
 
-# Execute!
-env.run(until=SIM_TIME)
+if __name__ == '__main__':
+    # Setup and start the simulation
+    print('Gas Station refuelling')
+    random.seed(RANDOM_SEED)
+
+    # Create environment and start processes
+    env = simpy.Environment()
+    gas_station = simpy.Resource(env, 2)
+    fuel_pump = simpy.Container(env, GAS_STATION_SIZE, init=GAS_STATION_SIZE)
+    env.process(gas_station_control(env, fuel_pump))
+    env.process(car_generator(env, gas_station, fuel_pump))
+
+    # Execute!
+    env.run(until=SIM_TIME)

fdd717da9581369d2044dc97113e4107330d4054
Zuyuan Zhu <zuyuanzhu@gmail.com>
2021-03-11 20:14:23 +0000

    minor tidy

--- topological_simpy/scripts/simple_topo_sim.py
+++ topological_simpy/scripts/simple_topo_sim.py
@@ -4,21 +4,20 @@ from topological_simpy.robot import Robot, TopoMap
 import simpy
 import itertools
 
-
 from random import randint, choice
 
 env = simpy.Environment()
-#tmap = TopoMap('/home/mhanheide/workspace/topological_navigation_ws/src/topological_navigation/topological_navigation/maps/test.tmap2', env)
+# tmap = TopoMap('/home/mhanheide/workspace/topological_navigation_ws/src/topological_navigation/topological_navigation/maps/test.tmap2', env)
 tmap = TopoMap('/home/zuyuan/catkin_ws/src/topological_navigation/topological_navigation/maps/riseholme.tmap2', env)
 
-#r1 = Robot('thorvald', tmap, 'A')
-#r1.goto('A')
-#r2 = Robot('thorvald2', tmap, 'D')
-#r2.goto('A')
+# r1 = Robot('thorvald', tmap, 'A')
+# r1.goto('A')
+# r2 = Robot('thorvald2', tmap, 'D')
+# r2.goto('A')
 
 robots = [
-    #Robot('Hurga', tmap, 'A'),
-    #Robot('Foo', tmap, 'B'),
+    # Robot('Hurga', tmap, 'A'),
+    # Robot('Foo', tmap, 'B'),
     Robot('Hurga', tmap, 'dock_0'),
     Robot('Foo', tmap, 'dock_1'),
     Robot('Foo2', tmap, 'dock_2')
@@ -28,13 +27,16 @@ nodes = tmap.get_nodes()
 
 print(nodes)
 
+
 def goal_generator(env, robots, nodes, max_interval=50):
     for i in itertools.count():
-        yield env.timeout(randint(env.now+1, env.now+max_interval))
+        delay_time = randint(env.now + 1, env.now + max_interval)
+        yield env.timeout(delay_time)
         r = choice(robots)
         n = choice(nodes)
         print('new goal for robot %s: %s' % (r._name, n))
         r.goto(n)
+
 
 env.process(goal_generator(env, robots, nodes))
 
@@ -50,4 +52,4 @@ while env.peek() < until:
         ))
     env.step()
 print(tmap._node_log)
-#env.run(until=3600)
+# env.run(until=3600)

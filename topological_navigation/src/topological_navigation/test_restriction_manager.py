e2adb5a6839f8c6216685c0752fabcfc0a822182
francescodelduchetto <francescodelduchetto@gmail.com>
2021-03-17 18:53:43 +0000

    restriction manager works with runtime and planning restrictions; test script for testing

---
+++ topological_navigation/src/topological_navigation/test_restriction_manager.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+import yaml
+from topological_navigation_msgs.srv import RestrictMap, RestrictMapRequest, RestrictMapResponse
+
+rospy.init_node("test")
+
+rospy.sleep(1)
+
+##
+srvpr = rospy.ServiceProxy("/restrictions_manager/restrict_planning_map", RestrictMap)
+
+req = RestrictMapRequest()
+req.state = '{"robot": "short", "task": "uv"}'
+
+res = srvpr.call(req)
+print("planning restrictions", res.success, len(yaml.safe_load(res.restricted_tmap)["nodes"]))
+rospy.sleep(1)
+
+##
+srvpr = rospy.ServiceProxy("/restrictions_manager/restrict_runtime_map", RestrictMap)
+
+req = RestrictMapRequest()
+req.state = '{"robot": "tall", "task": "uv"}'
+
+res = srvpr.call(req)
+
+print("runtime restrictions", res.success, len(yaml.safe_load(res.restricted_tmap)["nodes"]))

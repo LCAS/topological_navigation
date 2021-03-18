5a72386af0532cfd45489a6fd37735ac4b5b7f2e
Zuyuan Zhu <zuyuanzhu@gmail.com>
2021-03-18 15:23:42 +0000

    specify the robot home positions and target nodes for test

--- topological_simpy/scripts/simple_topo_sim.py
+++ topological_simpy/scripts/simple_topo_sim.py
@@ -19,12 +19,15 @@ tmap = TopoMap('/home/zuyuan/catkin_ws/src/topological_navigation/topological_na
 # r2 = Robot('thorvald2', tmap, 'D')
 # r2.goto('A')
 
+#robot_homes = ['dock_0', 'dock_0', 'dock_0']
+robot_homes = ['WayPoint131', 'WayPoint111', 'WayPoint66']
+target_nodes = ['WayPoint66', 'WayPoint142', 'WayPoint102']
 robots = [
     # Robot('Hurga', tmap, 'A'),
     # Robot('Foo', tmap, 'B'),
-    Robot('Hurga', tmap, 'dock_0'),
-    Robot('Foo', tmap, 'dock_1'),
-    Robot('Foo2', tmap, 'dock_2')
+    Robot('Hurga', tmap, robot_homes[0]),
+    Robot('Foo', tmap, robot_homes[1]),
+    Robot('Foo2', tmap, robot_homes[2])
 ]
 
 nodes = tmap.get_nodes()
@@ -32,9 +35,10 @@ nodes = tmap.get_nodes()
 print(nodes)
 
 
-def goal_generator(env, robots, nodes, max_interval=50):
+def goal_generator(env, robots, nodes, max_interval=200):
     for i in itertools.count():
-        delay_time = randint(env.now + 1, env.now + max_interval)  # TODO decrease delay_time
+        #delay_time = randint(env.now + 1, env.now + max_interval)  # TODO decrease delay_time
+        delay_time = randint(100, max_interval)  # TODO decrease delay_time
         print('.% 4d:    Generating goal after %6d seconds at %6d s' % (env.now, delay_time, env.now + delay_time))
         yield env.timeout(delay_time)
         r = choice(robots)
@@ -42,15 +46,13 @@ def goal_generator(env, robots, nodes, max_interval=50):
         print('+% 4d:    new goal for robot %10s: %s' % (env.now, r._name, n))
         r.goto(n)
 
-def goal_generator2(env, robots, nodes, max_interval=10):
+
+def goal_generator2(env, robots, target_nodes):
     for i in range(3):
-        #delay_time = randint(env.now + 1, env.now + max_interval)  # TODO decrease delay_time
-       # print('.% 4d:    Generating goal after %6d seconds at %6d s' % (env.now, delay_time, env.now + delay_time))
         yield env.timeout(5)
         r = robots[i]
-        n = nodes[3+i]
-        print('+% 4d:    new goal for robot %10s: %s' % (env.now, r._name, n))
-        r.goto(n)
+        print('+% 4d:    new goal for robot %10s: %s' % (env.now, r._name, target_nodes[i]))
+        r.goto(target_nodes[i])
 
 
 

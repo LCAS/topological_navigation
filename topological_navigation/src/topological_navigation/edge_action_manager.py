0bc0dc94f6e15a45ae19b27f1869309d94a8275c
adambinch <abinch@sagarobotics.com>
2021-04-19 09:37:17 +0100

    Improvements to the edge action manager

--- topological_navigation/src/topological_navigation/edge_action_manager.py
+++ topological_navigation/src/topological_navigation/edge_action_manager.py
@@ -5,13 +5,41 @@ Created on Tue Apr 13 22:02:24 2021
 
 """
 #########################################################################################################
-import rospy, actionlib
-from rospy_message_converter import message_converter
-import operator
-from copy import deepcopy
+import rospy, actionlib, operator, collections
 from functools import reduce  # forward compatibility for Python 3
+from rospy_message_converter import message_converter
 
 
+def _import(location, object_name):
+    mod = __import__(location, fromlist=[object_name]) 
+    return getattr(mod, object_name) 
+
+
+class dict_tools(object):
+    
+    
+    def nested_dict_iter(self, nested, prefix=""):
+        
+        for key, value in nested.iteritems():
+            path = "{},{}".format(prefix, key)
+            if isinstance(value, collections.Mapping):
+                for inner_key, inner_value in self.nested_dict_iter(value, path):
+                    yield inner_key, inner_value
+            else:
+                yield path[1:].split(","), value
+    
+    
+    def getFromDict(self, dataDict, mapList):
+        return reduce(operator.getitem, mapList, dataDict)
+
+
+    def setInDict(self, dataDict, mapList, value):
+        self.getFromDict(dataDict, mapList[:-1])[mapList[-1]] = value
+        return dataDict
+#########################################################################################################
+
+
+#########################################################################################################
 class EdgeActionManager(object):
     
     
@@ -30,52 +58,30 @@ class EdgeActionManager(object):
         action_spec = items[1][:-4] + "Action"
         
         rospy.loginfo("Edge Action Manager: Importing {} from {}.msg".format(action_spec, package))
-        action = self._import(package + ".msg", action_spec)
+        action = _import(package + ".msg", action_spec)
+        
+        rospy.loginfo("Edge Action Manager: Constructing the goal")
+        self.construct_goal(action_type, edge["goal"])
         
         rospy.loginfo("Edge Action Manager: Creating a {} client".format(self.action_name))
         self.client = actionlib.SimpleActionClient(self.action_name, action)        
         self.client.wait_for_server()
         
-        rospy.loginfo("Edge Action Manager: Constructing the goal")
-        self.construct_goal(action_type, edge["goal"])
-        
-        
-    def _import(self, location, object_name):
-        mod = __import__(location, fromlist=[object_name]) 
-        return getattr(mod, object_name) 
-        
         
     def construct_goal(self, action_type, goal_args):
         
-        goal_args_cpy = deepcopy(goal_args)
-        stack = list(goal_args_cpy.items()) 
+        dt = dict_tools()
+        paths = list(dt.nested_dict_iter(goal_args))
         
-        visited = set() 
-        keys = []
-        while stack: 
-            k, v = stack.pop() 
-            if isinstance(v, dict): 
-                keys.append(k)
-                if k not in visited: 
-                    stack.extend(v.items()) 
-            else:
-                keys.append(k)
-                if isinstance(v, str) and v.startswith("$"):
-                    _property = self.getFromDict(self.destination_node, v[1:].split("."))
-                    self.setInDict(goal_args, keys, _property)
-                
-                keys = [keys[0]]
-            visited.add(k)
+        for path in paths:
+            keys = path[0]
+            value = path[1]
+        
+            if isinstance(value, str) and value.startswith("$"):
+                _property = dt.getFromDict(self.destination_node, value[1:].split("."))
+                goal_args = dt.setInDict(goal_args, keys, _property)
         
         self.goal = message_converter.convert_dictionary_to_ros_message(action_type, goal_args)
-        
-        
-    def getFromDict(self, dataDict, mapList):
-        return reduce(operator.getitem, mapList, dataDict)
-
-
-    def setInDict(self, dataDict, mapList, value):
-        self.getFromDict(dataDict, mapList[:-1])[mapList[-1]] = value
         
         
     def execute(self):

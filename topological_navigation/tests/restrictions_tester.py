9910672d2b1580c5e9ed8e72431c52c930bf7b4e
francescodelduchetto <francescodelduchetto@gmail.com>
2021-09-09 19:21:12 +0100

    wait for service before calling it, and remove check of topics given it's already done in the .test file

--- topological_navigation/tests/restrictions_tester.py
+++ topological_navigation/tests/restrictions_tester.py
@@ -8,41 +8,15 @@ from topological_navigation.route_search2 import TopologicalRouteSearch2
 from topological_navigation_msgs.srv import RestrictMap, RestrictMapRequest, RestrictMapResponse
 
 class TestRestrictions(unittest.TestCase):
-
-    def restr_tmap_cb(self, msg):
-        self.restr_tmap = yaml.safe_load(msg.data)
-
-    def tmap_cb(self, msg):
-        self.tmap = yaml.safe_load(msg.data)
     
     # only functions with 'test_'-prefix will be run!
     def setUp(self):
         rospy.init_node("restrictions_tester")
 
-        rospy.Subscriber("/restricted_topological_map_2", String, self.restr_tmap_cb)
-
-        rospy.Subscriber("/topological_map_2", String, self.tmap_cb)
-
-        self.tmap = None
-        self.restr_tmap = None
-
-    def test_maps_received(self):
-
-        rate = rospy.Rate(1) # check every second
-        max_seconds = 10
-        count = 0
-        
-        while not rospy.is_shutdown() and count < max_seconds and\
-                (self.restr_tmap is None or self.tmap is None): 
-            count += 1
-            rate.sleep()
-
-        self.assertTrue(self.restr_tmap is not None)
-        self.assertTrue(self.tmap is not None)
-
     def test_restriction_tall_robot(self):
-
         # restrict the planning map for a tall robot
+        rospy.wait_for_service("/restrictions_manager/restrict_planning_map", timeout=30)
+
         srvpr = rospy.ServiceProxy("/restrictions_manager/restrict_planning_map", RestrictMap)
 
         req = RestrictMapRequest()
@@ -68,6 +42,8 @@ class TestRestrictions(unittest.TestCase):
 
     def test_restriction_short_robot(self):
         # restrict the planning map for a tall robot
+        rospy.wait_for_service("/restrictions_manager/restrict_planning_map", timeout=30)
+        
         srvpr = rospy.ServiceProxy("/restrictions_manager/restrict_planning_map", RestrictMap)
 
         req = RestrictMapRequest()

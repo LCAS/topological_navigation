5d696a4e85eb85b67670d7e10726a9ee8527c200
James R Heselden <primordia@live.com>
2023-06-22 18:49:26 +0200

    topological transform publisher

--- topological_navigation/topological_navigation/scripts/topological_transform_publisher.py
+++ topological_navigation/topological_navigation/scripts/topological_transform_publisher.py
@@ -1,68 +1,57 @@
-#!/usr/bin/env python
-"""
-Created on Fri Feb 26 10:37:50 2021
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# ----------------------------------
+# @author: Adam Binch
+# @email: abinch@sagarobotics.com
+# @date: Fri Feb 26 10:37:50 2021
+# ----------------------------------
 
-@author: Adam Binch (abinch@sagarobotics.com)
-"""
-#########################################################################################################
-import rospy, json
-from std_msgs.msg import String
-from geometry_msgs.msg import Vector3, Quaternion, TransformStamped
-from rospy_message_converter import message_converter
+import json
+
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile, DurabilityPolicy
 import tf2_ros
 
+from std_msgs.msg import String
+from geometry_msgs.msg import Vector3, Quaternion, TransformStamped
 
-class TopologicalTransformPublisher(object):
-    """This class defines a tf publisher that publishes the transformation
-    between parent and child frames of a topological_map_2.
-    """
+
+class TopologicalTransformPublisher(Node):
+
     def __init__(self):
-
-        self.rec_map=False
-
-        self.broadcaster = tf2_ros.StaticTransformBroadcaster()
-
-        rospy.Subscriber('topological_map_2', String, self.map_callback)
-
-        rospy.loginfo("Transform Publisher waiting for the Topological Map...")
-        while not self.rec_map :
-            rospy.sleep(rospy.Duration.from_sec(0.1))
-
+        super().__init__("topological_transform_publisher")
+        self.tf_broadcaster = tf2_ros.static_transform_broadcaster.StaticTransformBroadcaster(self)
+        qos = QoSProfile(depth=1, durability=DurabilityPolicy.TRANSIENT_LOCAL)
+        self.create_subscription(String, '/topological_map_2', self.map_callback, qos)
+        self.get_logger().info("Transform Publisher waiting for the Topological Map...")
 
     def map_callback(self, msg):
-
-        self.tmap = json.loads(msg.data)
-        self.rec_map=True
-
-        rospy.loginfo("Transform Publisher received the Topological Map")
-
-        self.publish_transform()
-
-
-    def publish_transform(self):
-
-        transformation = self.tmap["transformation"]
-
-        trans = message_converter.convert_dictionary_to_ros_message("geometry_msgs/Vector3", transformation["translation"])
-        rot = message_converter.convert_dictionary_to_ros_message("geometry_msgs/Quaternion", transformation["rotation"])
-
+        tmap = json.loads(msg.data)
+        self.get_logger().info("Transform Publisher received the Topological Map")
+        transformation = tmap["transformation"]
         msg = TransformStamped()
-        msg.header.stamp = rospy.Time.now()
+        msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = transformation["parent"]
         msg.child_frame_id = transformation["child"]
-        msg.transform.translation = trans
-        msg.transform.rotation = rot
+        msg.transform.translation.x = transformation["translation"]["x"]
+        msg.transform.translation.y = transformation["translation"]["y"]
+        msg.transform.translation.z = transformation["translation"]["z"]
+        msg.transform.rotation.x = transformation["rotation"]["x"]
+        msg.transform.rotation.y = transformation["rotation"]["y"]
+        msg.transform.rotation.z = transformation["rotation"]["z"]
+        msg.transform.rotation.w = transformation["rotation"]["w"]
+        self.tf_broadcaster.sendTransform(msg)
 
-        self.broadcaster.sendTransform(msg)
 
-#########################################################################################################
+def main(args=None):
+    rclpy.init(args=args)
 
+    TTP = TopologicalTransformPublisher()
+    rclpy.spin(TTP)
 
-#########################################################################################################
+    TTP.destroy_node()
+    rclpy.shutdown()
+
 if __name__ == '__main__':
-
-    rospy.init_node("topological_transform_publisher")
-
-    TopologicalTransformPublisher()
-    rospy.spin()
-#########################################################################################################
+    main()
